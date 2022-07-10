5b2fb7a204cbb6b9fb911805da2af2707359569b
GhaythBenAbid <ghaythbenabid0@gmail.com>
2022-07-10 15:21:29 +0100

    fix issue #1

--- WifiPassword.py
+++ WifiPassword.py
@@ -8,12 +8,15 @@ for router in routers_data:
     if(":" in router):
         routers.append(router[router.index(":") + 2:-1])
 
-
 passwords = []
-for index in range(1,len(routers)):
-    password_data = subprocess.check_output(["netsh" , "wlan" , "show" , "profile" , routers[index] , "key=clear"]).decode('utf-8' , errors="backslashreplace").split('\n')
-    password_text = password_data[32]
-    passwords.append(password_text[password_text.index(":") + 2:-1])
+for router in routers:
+    if(router != ""):
+        password_data = subprocess.check_output(["netsh" , "wlan" , "show" , "profile" , router , "key=clear"]).decode('utf-8' , errors="backslashreplace").split('\n')
+        password_text = password_data[32]
+        try:
+            passwords.append(password_text[password_text.index(":") + 2:-1])
+        except:
+            passwords.append("No password found")
 
 for index in range(1,len(routers)):
     print(routers[index] ," => " , passwords[index-1])
